1841b6ebf9ca12bfb353cb267356c9de7cb3cc21
niftymonk <info@niftymonk.com>
2026-03-12 21:56:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,16 @@ def home():
 def screener():
     try:
         s = requests.Session()
-        r = s.get("https://chartink.com/screener/daily-green-candle", timeout=15)
+        r = s.get("https://chartink.com/screener/bullish-pullback-buy-signal", timeout=15)
         m = re.search(r'csrf-token" content="(.+?)"', r.text)
         h = {
-            "Referer": "https://chartink.com/screener/daily-green-candle",
+            "Referer": "https://chartink.com/screener/bullish-pullback-buy-signal",
             "X-Requested-With": "XMLHttpRequest",
             "Content-Type": "application/x-www-form-urlencoded"
         }
         if m:
             h["X-CSRF-TOKEN"] = m.group(1)
-        p = {"scan_clause": "( {cash} ( latest close > latest open ) )"}
+        p = {"scan_clause": "( {46553} (  1 day ago close >  1 day ago open and  daily close <  daily open and  daily high <=  1 day ago high and  daily low >=  1 day ago low and  daily close <=  1 day ago high -  (  (  1 day ago high -  1 day ago low ) *  0.5 ) and  daily close >=  1 day ago high -  (  (  1 day ago high -  1 day ago low ) *  0.6 ) ) )"}
         res = s.post("https://chartink.com/screener/process", data=p, headers=h, timeout=15)
         d = res.json()
         stocks = d.get("data", [])
